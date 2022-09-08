daa0d9be5d276cd80b2d574c3388f5ca49b98ee1
root <hungcao0402@gmail.com>
2022-09-08 14:50:45 +0000

    up

--- scripts/download_infer.sh
+++ scripts/download_infer.sh
@@ -3,3 +3,5 @@ gdown 1ekWidv8aEWPudEdQAMPwmVH48h92_w5j #yolov7
 gdown 1w5qH7C5oZPZGidfaxqvV4F6H55NXYCSQ #vietocr
 gdown 101idb3KMivpacu0joTaIUFnpRh-TL4OM #classify
 gdown 1wX70t-AkxsW-kW_uAokD7JrmsXWf0uNM #DINO
+cd DINO 
+gdown 1V-1rMKxQBYwpz77R6axzzU13yaQdu4MU -O pretrained
