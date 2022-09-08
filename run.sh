4147bfca5cd064cb701e5809623c76a11fe9d4e4
root <hungcao0402@gmail.com>
2022-09-08 15:42:42 +0000

    up

--- run.sh
+++ run.sh
@@ -6,12 +6,12 @@ pres_image_path="${test_path}/prescription/image"
 CUDA_VISIBLE_DEVICES=$2 python3 yolov7/detect_ocr.py --weights ./weights/yolov7_ocr.pt   \
         --no-trace --conf 0.2 --save-txt --img-size 640   \
          --source $pres_image_path  \
-         --project /tempt/ocr --weights_ocr ./weights/vietocr.pt
+         --project /temp/ocr --weights_ocr ./weights/vietocr.pt
 
 # # # Detect pill
 cd DINO 
 CUDA_VISIBLE_DEVICES=$2 python inference.py --source $pill_image_path   \
-                --output '/tempt/detect' \
+                --output '/temp/detect' \
                 --thershold 0.4 \
                 --model_config_path ./config/DINO/infer.py      \
                 --model_checkpoint_path ../weights/checkpoint_best_regular.pth
@@ -20,9 +20,9 @@ cd ..
 #Classify
 cd classify 
 CUDA_VISIBLE_DEVICES=$2 python infer_vote.py --c ./config/infer_108class.yaml \
-        --csv_path /tempt/detect/output.csv   \
-        --input_root /tempt/detect/crop_img       \
-        --ocr_root /tempt/ocr/exp/labels \
+        --csv_path /temp/detect/output.csv   \
+        --input_root /temp/detect/crop_img       \
+        --ocr_root /temp/ocr/exp/labels \
         --output ../output/results.csv
 
 cd ../output && zip results.zip results.csv
